f105c4b225e0495bfe1827c3c7454164b0970fde
Monil Narang <monil.narang@olx.com>
2021-02-01 14:38:07 +0530

    updated Roles.java

---
+++ src/main/java/com/backend/heArt/entity/Roles.java
@@ -0,0 +1,19 @@
+package com.backend.heArt.entity;
+
+public enum Roles {
+    ROLE_ADMIN("Admin", 1), ROLE_USER("User", 2);
+
+    String roleName;
+    int roleId;
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    Roles(String admin, int i) {
+    }
+}
